6e7b783aa80816bc3699733f01aac9c787d51503
Mihik197 <gea1500517@gmail.com>
2025-04-27 02:32:05 +0530

    fix: correct tensor initialization and padding token checks in BilingualDataset

--- attention_is_all_you_need/dataset.py
+++ attention_is_all_you_need/dataset.py
@@ -14,9 +14,9 @@ class BilingualDataset(Dataset):
         self.target_lang = target_lang
         self.seq_length = seq_length
 
-        self.sos_token = torch.Tensor([tokenizer_src.token_to_id(['[SOS]'])], dtype=torch.int64)
-        self.eos_token = torch.Tensor([tokenizer_src.token_to_id(['[EOS]'])], dtype=torch.int64)
-        self.pad_token = torch.Tensor([tokenizer_src.token_to_id(['[PAD]'])], dtype=torch.int64)
+        self.sos_token = torch.tensor([tokenizer_src.token_to_id('[SOS]')], dtype=torch.int64)
+        self.eos_token = torch.tensor([tokenizer_src.token_to_id('[EOS]')], dtype=torch.int64)
+        self.pad_token = torch.tensor([tokenizer_src.token_to_id('[PAD]')], dtype=torch.int64)
 
     def __len__(self):
         return len(self.ds)
@@ -32,7 +32,7 @@ class BilingualDataset(Dataset):
         enc_num_padding_tokens = self.seq_length - len(enc_input_tokens) - 2  # 2 for SOS and EOS
         dec_num_padding_tokens = self.seq_length - len(dec_input_tokens) - 1  # 1 for SOS
 
-        if enc_input_tokens < 0 or  dec_input_tokens < 0:
+        if enc_num_padding_tokens < 0 or  dec_num_padding_tokens < 0:
             raise ValueError('Sentence is too long')
         
         # SOS, source_text, EOS, PADDING
